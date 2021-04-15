1c6a981497049e4b563e4b5deb7e1661d832ac11
Candice PRIVAT <72886468+candiceprivat@users.noreply.github.com>
2021-04-15 17:49:45 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,14 +15,14 @@ let animalButton;
 
 function modelReady() {
   console.log('Model is ready!!!');
-  // classifier.load('model.json', customModelReady);
+  classifier.load('model.json', customModelReady);
 }
 
-// function customModelReady() {
-//   console.log('Custom Model is ready!!!');
-//   label = 'model ready';
-//   classifier.classify(gotResults);
-// }
+function customModelReady() {
+console.log('Custom Model is ready!!!');
+ label = 'model ready';
+classifier.classify(gotResults);
+ }
 
 function videoReady() {
   console.log('Video is ready!!!');
@@ -36,31 +36,31 @@ function setup() {
   mobilenet = ml5.featureExtractor('MobileNet', modelReady);
   classifier = mobilenet.classification(video, videoReady);
 
-  humanButton = createButton('human');
-  humanButton.mousePressed(function() {
-    classifier.addImage('human');
-  });
+//  humanButton = createButton('human');
+  //humanButton.mousePressed(function() {
+    //classifier.addImage('human');
+  //});
 
-  buildingButton = createButton('building');
-  buildingButton.mousePressed(function() {
-    classifier.addImage('building');
-  });
+  //buildingButton = createButton('building');
+  //buildingButton.mousePressed(function() {
+    //classifier.addImage('building');
+  //});
    
-  animalButton = createButton('animal');
-  animalButton.mousePressed(function() {
-    classifier.addImage('animal');
-  });
+  //animalButton = createButton('animal');
+  //animalButton.mousePressed(function() {
+   // classifier.addImage('animal');
+  //});
   
-  trainButton = createButton('train');
-  trainButton.mousePressed(function() {
-    classifier.train(whileTraining);
-  });
+  //trainButton = createButton('train');
+ // trainButton.mousePressed(function() {
+  //  classifier.train(whileTraining);
+ // });
 
-  saveButton = createButton('save');
-  saveButton.mousePressed(function() {
-    classifier.save();
-  });
-}
+ //saveButton = createButton('save');
+ // saveButton.mousePressed(function() {
+   // classifier.save();
+  //});
+//}
 
 function draw() {
   background(0);
@@ -70,14 +70,14 @@ function draw() {
   text(label, 10, height - 10);
 }
 
-function whileTraining(loss) {
-  if (loss == null) {
-    console.log('Training Complete');
-    classifier.classify(gotResults);
-  } else {
-    console.log(loss);
-  }
-}
+//function whileTraining(loss) {
+//  if (loss == null) {
+ //   console.log('Training Complete');
+  //  classifier.classify(gotResults);
+  //} else {
+  //  console.log(loss);
+//  }
+//}
 
 function gotResults(error, result) {
   if (error) {
